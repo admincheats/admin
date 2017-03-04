fe92dfee529b638a755c92cd6a64620d40ccec7e
mwear3robloxandroid <mitchellwears@gmail.com>
2017-03-03 20:01:02 -0500

    Update chat.java

--- chat.java
+++ chat.java
@@ -2,9 +2,9 @@ workspace.Dialog.DialogChoiceSelected:connect(function(player,choice)
 	print(player.Name,choice.Name)
 end)
 workspace.Dialog.DialogChoiceSelected:connect(function(player,choice)
-	if choice.Name == "No" then
+	if choice.Name == "hello im admin" then
 		player.Character.Humanoid.Health = 0
-	elseif choice.Name == "Yes" then
+	elseif choice.Name == "i was made by mwear3" then
 		player.Character.Humanoid.Health = 1000
 	end
 end)
